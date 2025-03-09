44145e3edd83d7aa6851b4bd618a3d397b03ee43
jcohen <jcohen@student.42.fr>
2025-03-10 00:41:40 +0100

    fix warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Box, Flex, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { ThemeProvider } from "next-themes";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "42Pong",
@@ -18,21 +19,42 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          id="theme-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                let theme = localStorage.getItem('theme');
+                if (!theme) theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+                document.documentElement.classList.add(theme);
+                document.documentElement.style.colorScheme = theme;
+              } catch (e) {}
+            `,
+          }}
+        />
+      </head>
       <body>
         <AuthProvider>
-			<ThemeProvider attribute="class" enableSystem>
-				<Theme accentColor="blue" appearance="inherit" grayColor="slate" scaling="100%" radius="medium">
-					<Protected>
-						<Flex style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
-							<Sidebar />
-							<div style={{ width: '100%', height: '100%', padding: '20px', backgroundColor: 'var(--gray-2)', overflow: 'auto' }}>
-								{children}
-							</div>
-						</Flex>
-					</Protected>
-				</Theme>
-		    </ThemeProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem={true}
+            value={{ light: 'light', dark: 'dark' }}
+          >
+            <Theme accentColor="blue" appearance="inherit" grayColor="slate" scaling="100%" radius="medium">
+              <Protected>
+                <Flex style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+                  <Sidebar />
+                  <div style={{ width: '100%', height: '100%', padding: '20px', backgroundColor: 'var(--gray-2)', overflow: 'auto' }}>
+                    {children}
+                  </div>
+                </Flex>
+              </Protected>
+            </Theme>
+          </ThemeProvider>
         </AuthProvider>
       </body>
     </html>
