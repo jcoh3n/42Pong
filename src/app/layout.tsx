07bc04cfd11d62bb686b91a380cb85ab3f64a546
idrissa <utileidrissa@gmail.com>
2025-03-09 23:46:15 +0100

    added system theme by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Protected from "@/components/Protected";
 import { Box, Flex, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import Sidebar from "@/components/sidebar/Sidebar";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "42Pong",
@@ -20,16 +21,18 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AuthProvider>
-          <Theme accentColor="blue" appearance="dark" grayColor="slate" scaling="100%" radius="medium">
-            <Protected>
-				<Flex style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
-					<Sidebar />
-					<div style={{ width: '100%', height: '100%', padding: '20px', backgroundColor: 'var(--gray-2)', overflow: 'auto' }}>
-						{children}
-					</div>
-				</Flex>
-			</Protected>
-          </Theme>
+		<ThemeProvider attribute="class" enableSystem>
+			<Theme accentColor="blue" grayColor="slate" scaling="100%" radius="medium">
+				<Protected>
+					<Flex style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+						<Sidebar />
+						<div style={{ width: '100%', height: '100%', padding: '20px', backgroundColor: 'var(--gray-2)', overflow: 'auto' }}>
+							{children}
+						</div>
+					</Flex>
+				</Protected>
+			</Theme>
+		  </ThemeProvider>
         </AuthProvider>
       </body>
     </html>
