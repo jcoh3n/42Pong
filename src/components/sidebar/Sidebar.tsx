291e4d54a823fdb0220a0defed9cfccca5671ee2
idrissa <utileidrissa@gmail.com>
2025-03-09 21:24:46 +0100

    bigger sidebar | remove home buttons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
   return (
     <Box
       style={{ 
-        width: '220px', 
+        width: '300px', 
         height: '100vh',
         borderRight: '1px solid var(--gray-5)',
         display: 'flex',
@@ -25,22 +25,22 @@ export default function Sidebar() {
       }}
     >
       {/* Logo */}
-      <Flex align="center" py="4" px="4">
+      <Flex align="center" py="6" px="6">
         <Link href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <Flex align="center" gap="2">
+          <Flex align="center" gap="4">
             <Box style={{ 
               display: 'flex', 
               alignItems: 'center', 
               justifyContent: 'center',
-              width: '32px',
-              height: '32px'
+              width: '40px',
+              height: '30px'
             }}>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect width="24" height="24" rx="6" fill="black" />
                 <path d="M7 13L10 10M10 10L7 7M10 10H16.5M14 7L17 10M17 10L14 13M17 10H10.5" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             </Box>
-            <Text weight="bold" size="3">42Pong</Text>
+            <Text weight="bold" size="5">42Pong</Text>
           </Flex>
         </Link>
       </Flex>
@@ -48,34 +48,34 @@ export default function Sidebar() {
       <Separator size="4" />
       
       {/* Main Navigation */}
-      <Box py="2" style={{ flex: 1 }}>
-        <Flex direction="column" gap="1">
+      <Box py="3" style={{ flex: 1 }}>
+        <Flex direction="column" gap="2">
           <SidebarNavItem 
-            icon={<HomeIcon width="16" height="16" />} 
+            icon={<HomeIcon width="20" height="20" />} 
             label="Home" 
             href="/" 
           />
           <SidebarNavItem 
-            icon={<PersonIcon width="16" height="16" />} 
-            label="Profile" 
-            href="/profile" 
+            icon={<DashboardIcon width="20" height="20" />} 
+            label="Dashboard" 
+            href="/dashboard" 
             hasAddButton 
             onAddClick={() => console.log("Add dashboard")} 
           />
           <SidebarNavItem 
-            icon={<ListBulletIcon width="16" height="16" />} 
-            label="Games"
-            href="/games"
+            icon={<ListBulletIcon width="20" height="20" />} 
+            label="Games" 
+            href="/games" 
             hasAddButton 
             onAddClick={() => console.log("Add game")} 
           />
           <SidebarNavItem 
-            icon={<ListBulletIcon width="16" height="16" />} 
+            icon={<PersonIcon width="20" height="20" />} 
             label="Leaderboard" 
             href="/leaderboard" 
           />
           <SidebarNavItem 
-            icon={<GearIcon width="16" height="16" />} 
+            icon={<GearIcon width="20" height="20" />} 
             label="Settings" 
             href="/settings" 
           />
@@ -83,16 +83,16 @@ export default function Sidebar() {
       </Box>
       
       {/* Bottom Navigation */}
-      <Box py="2">
-        <Separator size="4" mb="2" />
-        <Flex direction="column" gap="1">
+      <Box py="3">
+        <Separator size="4" mb="3" />
+        <Flex direction="column" gap="2">
           <SidebarNavItem 
-            icon={<InfoCircledIcon width="16" height="16" />} 
+            icon={<InfoCircledIcon width="20" height="20" />} 
             label="Help & Information" 
             href="/help" 
           />
           <SidebarNavItem 
-            icon={<ExitIcon width="16" height="16" />} 
+            icon={<ExitIcon width="20" height="20" />} 
             label="Log out" 
             href="/api/auth/signout" 
           />
