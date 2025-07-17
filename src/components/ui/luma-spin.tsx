d8ae1c5c715b83c67ad382e772e4e1433f94dc38
idrissaa <165401497+iibabyy@users.noreply.github.com>
2025-07-17 05:11:57 +0000

    fixed build error

--- src/components/ui/luma-spin.tsx
+++ src/components/ui/luma-spin.tsx
@@ -4,7 +4,6 @@ export const Component = () => {
       <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
       <span className="absolute rounded-[50px] animate-loaderAnim animation-delay shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
       <style jsx>
-        {" "}
         {`
           @keyframes loaderAnim {
             0% {
